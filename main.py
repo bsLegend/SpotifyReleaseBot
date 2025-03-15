42fa1ecfcc9090457b66a6a42983da709eb4c693
bsLegend <algemein12@gmail.com>
2025-03-15 15:45:54 +0100

    fixed multiple notifications being sent

--- main.py
+++ main.py
@@ -18,6 +18,22 @@ except FileNotFoundError:
 except json.JSONDecodeError:
     print("Error: Failed to decode JSON. Please ensure 'artists.json' is properly formatted.")
 
+alreadyMessaged = []
+
+try:
+    with open("alreadyMessaged.json", "r") as file:
+        alreadyMessagedData = json.load(file)
+
+    if "alreadyMessaged" in alreadyMessagedData:
+        alreadyMessaged = alreadyMessagedData["alreadyMessaged"]
+    else:
+        raise KeyError("The JSON file does not contain a key named 'alreadyMessaged'. Please check the file structure.")
+
+except FileNotFoundError:
+    print("Error: The file 'alreadyMessaged.json' was not found. Please ensure the file exists in the correct directory.")
+except json.JSONDecodeError:
+    print("Error: Failed to decode JSON. Please ensure 'alreadyMessaged.json' is properly formatted.")
+
 
 TokenUrl = "https://accounts.spotify.com/api/token"
 
@@ -37,11 +53,16 @@ newReleaseData = {
 for artistId in artistIds:
     spotifyAPIUrl = f"https://api.spotify.com/v1/artists/{artistId}/albums"
     get_new_release(spotifyAPIUrl, TokenUrl, clientId, clientSecret, newReleaseData)
+    for items in alreadyMessaged:
+        if items == f"{newReleaseData['Artist']}{newReleaseData['AlbumName']}{newReleaseData['Type']}":
+            newReleaseData["newRelease"] = False
+            break
     if newReleaseData["newRelease"] is True:
         message = f"New Release from {newReleaseData['Artist']}:\n{newReleaseData['AlbumName']}\n{newReleaseData['Type']}"
         send_message(message)
 
+        alreadyMessagedData["alreadyMessaged"].append(
+            f"{newReleaseData['Artist']}{newReleaseData['AlbumName']}{newReleaseData['Type']}")
 
-
-
-
+        with open("alreadyMessaged.json", "w") as file:
+            json.dump(alreadyMessagedData, file, indent=4)
